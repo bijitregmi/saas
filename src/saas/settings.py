4f2835785d4dbe141f77f6886fe60b5a1dca0ffe
Bijit Regmi <104486517+bijitregmi@users.noreply.github.com>
2024-07-10 15:01:49 +0600

    Set debug environ variable

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&o+v6#in8%7gqk3&(1to8t_j2t5(3(g%dc&y8!#6yy7c8(atu$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 
 ALLOWED_HOSTS = [
     ".railway.app",
